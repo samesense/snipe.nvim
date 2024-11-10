512bc1fb69dcfa8be7a9308466f4fb6a42f3b56a
James-Evans <James-Evans@sparktx.com>
2024-11-10 15:42:35 -0500

    add snakefile and run

--- lua/snipe/buffer.lua
+++ lua/snipe/buffer.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 local Buffer = {
-  id = 0,
-  name = "",
-  classifiers = "     ", -- see :help ls for more info
+	id = 0,
+	name = "",
+	classifiers = "     ", -- see :help ls for more info
 }
 
 Buffer.__index = Buffer
@@ -12,24 +12,58 @@ M.Buffer = Buffer
 
 -- Converts single line from ":buffers" output
 function Buffer:from_line(s)
-  local o = setmetatable({}, Buffer)
+	local o = setmetatable({}, Buffer)
 
-  o.id = tonumber(vim.split(s, " ", {trimempty = true})[1])
-  o.classifiers = s:sub(4, 8)
+	o.id = tonumber(vim.split(s, " ", { trimempty = true })[1])
+	o.classifiers = s:sub(4, 8)
 
-  local ss = s:find('"')
-  local se = #s - s:reverse():find('"')
+	local ss = s:find('"')
+	local se = #s - s:reverse():find('"')
 
-  o.name = s:sub(ss + 1, se)
+	o.name = s:sub(ss + 1, se)
 
-  return o
+	return o
 end
 
 function M.get_buffers(cmd)
-  cmd = cmd or "ls"
-  local bufs_out = vim.api.nvim_exec2(cmd, { output = true }).output
-  local bufs = vim.split(bufs_out, "\n", { trimempty = true })
-  return vim.tbl_map(function (l) return Buffer:from_line(l) end, bufs)
+	cmd = cmd or "ls"
+	local bufs_out = vim.api.nvim_exec2(cmd, { output = true }).output
+	local bufs = vim.split(bufs_out, "\n", { trimempty = true })
+	local buffers = vim.tbl_map(function(l)
+		return Buffer:from_line(l)
+	end, bufs)
+
+	-- Check if run.sh exists and create a buffer entry if not present
+	local run_sh_exists = false
+	local snakefile_exists = false
+
+	for _, buf in ipairs(buffers) do
+		if buf.name == "run.sh" then
+			run_sh_exists = true
+		end
+		if buf.name == "Snakefile.py" then
+			snakefile_exists = true
+		end
+	end
+
+	-- Add run.sh buffer if not present
+	if not run_sh_exists then
+		local run_sh = setmetatable({}, Buffer)
+		run_sh.id = #buffers + 1
+		run_sh.name = "run.sh"
+		run_sh.classifiers = "     "
+		table.insert(buffers, run_sh)
+	end
+	-- Add Snakefile.py buffer if not present
+	if not snakefile_exists then
+		local snakefile = setmetatable({}, Buffer)
+		snakefile.id = #buffers + 1
+		snakefile.name = "Snakefile.py"
+		snakefile.classifiers = "     "
+		table.insert(buffers, snakefile)
+	end
+
+	return buffers
 end
 
 return M
